6616a6151cce0ec8b9d92eacddabc07ae119e213
Mykhailo Kalinichenko <99479355+fidelmigel@users.noreply.github.com>
2024-09-20 15:37:11 +0200

    transferred styles

--- script.js
+++ script.js
@@ -9,31 +9,49 @@ const dataItems = JSON.parse(getValue("data-items", fusifyTag.attributes));
 const autoSpeed = getValue("auto-speed", fusifyTag.attributes);
 
 function makeHTML() {
-  const firstFaceContant =
-    '<a href="' +
-    dataItems[0].link +
-    '" class="side front" target="_blank"><video src="images/tel.mp4" muted playsinline autoplay loop></video></a>';
-  const secondFaceContant =
-    '<a href="' +
-    dataItems[1].link +
-    '" class="side back" target="_blank"><img src="' +
-    dataItems[1]["second-face"] +
-    '"></a>';
-  const thirdFaceContant =
-    '<a href="' +
-    dataItems[2].link +
-    '" class="side left" target="_blank"></a>';
-  const forthFaceContant =
-    '<a href="' +
-    dataItems[3].link +
-    '" class="side right" target="_blank"></a>';
+  // Масив об'єктів з інформацією для кожної грані куба
+  const faces = [
+    {
+      class: "side front",
+      content:
+        '<video src="images/tel.mp4" muted playsinline autoplay loop></video>',
+      link: dataItems[0].link,
+    },
+    {
+      class: "side back",
+      content: '<img src="images/burger.png">',
+      link: dataItems[1].link,
+    },
+    {
+      class: "side left",
+      content: '<img src="images/chiken.png">',
+      link: dataItems[2].link,
+    },
+    {
+      class: "side right",
+      content: '<img src="images/meat.png">',
+      link: dataItems[3].link,
+    },
+  ];
+
+  // Генерація HTML для куба через map і join
   const htmlContent =
     '<div class="cube">' +
-    firstFaceContant +
-    secondFaceContant +
-    thirdFaceContant +
-    forthFaceContant +
+    faces
+      .map(
+        (face) =>
+          '<a href="' +
+          face.link +
+          '" class="' +
+          face.class +
+          '" target="_blank">' +
+          face.content +
+          "</a>"
+      )
+      .join("") +
     "</div>";
+
+  // Вставка згенерованого HTML у DOM
   fusifyTag.innerHTML = htmlContent;
 }
 
@@ -46,6 +64,89 @@ function getValue(name, attr) {
 
   return null;
 }
+
+function replaceCSS() {
+  // Стилізація body
+  const body = document.querySelector("body");
+  body.style.height = "100vh";
+  body.style.display = "flex";
+  body.style.justifyContent = "center";
+  body.style.alignItems = "center";
+  body.style.perspective = "1500px";
+  body.style.margin = "0";
+  body.style.padding = "0";
+  body.style.boxSizing = "border-box";
+
+  // Стилізація куба
+  const cube = document.querySelector(".cube");
+  cube.style.display = "block";
+  cube.style.width = "300px";
+  cube.style.height = "300px";
+  cube.style.position = "relative";
+  cube.style.transformStyle = "preserve-3d";
+  cube.style.willChange = "transform";
+
+  // Стилізація сторін куба
+  const sides = document.querySelectorAll(".side");
+  sides.forEach((side) => {
+    side.style.border = "2px solid #fff";
+    side.style.width = "100%";
+    side.style.height = "100%";
+    side.style.display = "flex";
+    side.style.justifyContent = "center";
+    side.style.alignItems = "center";
+    side.style.position = "absolute";
+    side.style.backfaceVisibility = "hidden";
+    side.style.backgroundSize = "cover";
+    side.style.backgroundPosition = "center";
+    side.style.textDecoration = "none";
+    side.style.willChange = "transform";
+  });
+
+  // Стилізація відео
+  const videos = document.querySelectorAll("video");
+  videos.forEach((video) => {
+    video.style.width = "100%";
+    video.style.height = "100%";
+    video.style.objectFit = "cover";
+    video.style.pointerEvents = "none";
+  });
+
+  // Стилізація кожної сторони куба
+  const front = document.querySelector(".front");
+  front.style.transform = "translateZ(150px)";
+
+  const back = document.querySelector(".back");
+  back.style.transform = "rotateY(180deg) translateZ(150px)";
+
+  const left = document.querySelector(".left");
+  left.style.transform = "rotateY(-90deg) translateZ(150px)";
+
+  const right = document.querySelector(".right");
+  right.style.transform = "rotateY(90deg) translateZ(150px)";
+
+  // Адаптивна стилізація для малих екранів
+  if (window.innerWidth <= 600) {
+    cube.style.width = "150px";
+    cube.style.height = "150px";
+    front.style.transform = "translateZ(75px)";
+    back.style.transform = "rotateY(180deg) translateZ(75px)";
+    left.style.transform = "rotateY(-90deg) translateZ(75px)";
+    right.style.transform = "rotateY(90deg) translateZ(75px)";
+  } else if (window.innerWidth <= 1024) {
+    cube.style.width = "200px";
+    cube.style.height = "200px";
+    front.style.transform = "translateZ(100px)";
+    back.style.transform = "rotateY(180deg) translateZ(100px)";
+    left.style.transform = "rotateY(-90deg) translateZ(100px)";
+    right.style.transform = "rotateY(90deg) translateZ(100px)";
+  }
+}
+
+// Виклик функції після завантаження сторінки
+window.onload = function () {
+  replaceCSS();
+};
 
 // Функція для автоматичного обертання з використанням requestAnimationFrame
 function startAutoRotate() {
